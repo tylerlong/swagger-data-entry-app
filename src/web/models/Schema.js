8dd783a952fa5d6255a26002df1011cfc82155ed
Tyler Long <tyler4long@gmail.com>
2017-11-14 19:26:03 +0800

    Add blf fixture

--- src/web/models/Schema.js
+++ src/web/models/Schema.js
@@ -5,7 +5,7 @@ import Property from './Property'
 const Schema = types.model({
   required: types.union(types.array(types.string), types.undefined),
   type: types.union(types.literal('object'), types.undefined),
-  properties: types.map(Property),
+  properties: types.union(types.map(Property), types.undefined),
   description: types.union(types.string, types.undefined)
 })
 
