33aab73959d635b20e86e8878cc6f3e9d79dd018
Tyler Long <tyler4long@gmail.com>
2017-11-15 16:23:54 +0800

    Test create swagger spec

--- src/web/models/swaggerStore.js
+++ src/web/models/swaggerStore.js
@@ -2,11 +2,17 @@ import { types } from 'mobx-state-tree'
 import yaml from 'js-yaml'
 
 import Swagger from './Swagger'
+import { toAndFromJson } from '../utils'
 
 const SwaggerFile = types.model({
   filePath: types.string,
   swagger: Swagger
-})
+}).actions(self => ({
+  save () {
+    const yamlData = yaml.safeDump(toAndFromJson(self.swagger.toJSON()))
+    global.fs.writeFileSync(self.filePath, yamlData)
+  }
+}))
 
 const SwaggerStore = types.model({
   swaggerFiles: types.array(SwaggerFile)
@@ -27,10 +33,12 @@ const SwaggerStore = types.model({
       },
       paths: {}
     })
-    self.swaggerFiles.push(SwaggerFile.create({
-      filePath,
-      swagger
-    }))
+    const swaggerFile = SwaggerFile.create({ filePath, swagger })
+    swaggerFile.save()
+    self.swaggerFiles.push(swaggerFile)
+  },
+  clear () {
+    self.swaggerFiles = []
   }
 }))
 
--- test/swaggerStore.spec.js
+++ test/swaggerStore.spec.js
@@ -1,7 +1,12 @@
 /* eslint-env jest */
 import path from 'path'
+import fs from 'fs'
 
 import swaggerStore from '../src/web/models/swaggerStore'
+
+beforeEach(() => {
+  swaggerStore.clear()
+})
 
 describe('swaggerStore', () => {
   test('open swagger spec', () => {
@@ -15,6 +20,16 @@ describe('swaggerStore', () => {
   })
 
   test('create swagger spec', () => {
-
+    expect(swaggerStore.swaggerFiles.length).toBe(0)
+    const filePath = path.join(__dirname, 'fixtures', 'does-not-exist.yml')
+    expect(fs.existsSync(filePath)).toBe(false)
+    swaggerStore.createSwaggerFile(filePath)
+    expect(swaggerStore.swaggerFiles.length).toBe(1)
+    const swaggerFile = swaggerStore.swaggerFiles[0]
+    expect(swaggerFile.filePath).toBe(filePath)
+    expect(swaggerFile.swagger.swagger).toBe('2.0')
+    expect(fs.existsSync(filePath)).toBe(true)
+    fs.unlinkSync(filePath)
+    expect(fs.existsSync(filePath)).toBe(false)
   })
 })
