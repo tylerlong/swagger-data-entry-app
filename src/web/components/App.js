1d692cd2e5196cbb29d0b5cd8e3e1069efa743fb
Tyler Long <tyler4long@gmail.com>
2017-11-15 17:29:24 +0800

    Make create button working

--- src/web/components/App.js
+++ src/web/components/App.js
@@ -15,13 +15,24 @@ class App extends React.Component {
     const filePath = filesOpened[0]
     swaggerStore.openSwaggerFile(filePath)
   }
+
+  onCreate () {
+    const filePath = global.electron.dialog.showSaveDialog({
+      filters: [{ name: 'swagger files', extensions: ['yml', 'yaml'] }]
+    })
+    if (!filePath) {
+      return
+    }
+    swaggerStore.createSwaggerFile(filePath)
+  }
+
   render () {
     return (
       <Tabs hideAdd type='editable-card'>
         <Tabs.TabPane key='home' closable={false} tab={<span><Icon type='home' /> Home</span>}>
           <Card title='Swagger specs' bordered={false}>
             <Button onClick={this.onOpen}>Open</Button>
-            <Button>New</Button>
+            <Button onClick={this.onCreate}>Create</Button>
           </Card>
         </Tabs.TabPane>
       </Tabs>
