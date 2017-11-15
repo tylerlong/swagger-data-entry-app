c185729390db52a8417d462cc258b3940ef7fc7b
Tyler Long <tyler4long@gmail.com>
2017-11-15 17:23:47 +0800

    Add action to open button

--- src/electron/preload.js
+++ src/electron/preload.js
@@ -1,5 +1,7 @@
 import fs from 'fs'
+import electron from 'electron'
 
 process.once('loaded', () => {
   global.fs = fs
+  global.electron = electron.remote
 })
--- src/web/components/App.js
+++ src/web/components/App.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { Tabs, Icon, Card, Button } from 'antd'
 
+import swaggerStore from '../models/swaggerStore'
+
 class App extends React.Component {
+  onOpen () {
+    const filesOpened = global.electron.dialog.showOpenDialog({
+      properties: ['openFile'],
+      filters: [{ name: 'swagger files', extensions: ['yml', 'yaml'] }]
+    })
+    if (!filesOpened) {
+      return
+    }
+    const filePath = filesOpened[0]
+    swaggerStore.openSwaggerFile(filePath)
+  }
   render () {
-    return <Tabs hideAdd type='editable-card'>
-      <Tabs.TabPane key='home' closable={false} tab={<span><Icon type='home' /> Home</span>}>
-        <Card title='Swagger specs' bordered={false}>
-          <Button>Open</Button>
-          <Button>New</Button>
-        </Card>
-      </Tabs.TabPane>
-    </Tabs>
+    return (
+      <Tabs hideAdd type='editable-card'>
+        <Tabs.TabPane key='home' closable={false} tab={<span><Icon type='home' /> Home</span>}>
+          <Card title='Swagger specs' bordered={false}>
+            <Button onClick={this.onOpen}>Open</Button>
+            <Button>New</Button>
+          </Card>
+        </Tabs.TabPane>
+      </Tabs>
+    )
   }
 }
 
