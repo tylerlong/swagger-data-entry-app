00a86ebce4827840170d13f2b9bbe0fa233029bb
Tyler Long <tyler4long@gmail.com>
2017-11-18 21:01:28 +0800

    Create Collapse

--- src/web/components/Definitions.js
+++ src/web/components/Definitions.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import { Card } from 'antd'
+import { Card, Collapse } from 'antd'
 
 class Definitions extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
   render () {
     return (
       <Card title='Definitions'>
-        Definitions
+        <Collapse accordion>
+          <Collapse.Panel header='hello header' key='1'>hello</Collapse.Panel>
+          <Collapse.Panel header='world header' key='2'>world</Collapse.Panel>
+        </Collapse>
       </Card>
     )
   }
