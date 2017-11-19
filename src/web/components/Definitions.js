3b7711e54b1bf6321e325c61708edebffab6badf
Tyler Long <tyler4long@gmail.com>
2017-11-19 10:30:12 +0800

    Can add definitions

--- src/web/components/Definitions.js
+++ src/web/components/Definitions.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { observer } from 'mobx-react'
-import { Card, Collapse, Button } from 'antd'
+import { Card, Collapse, Button, Icon } from 'antd'
 import uuidv1 from 'uuid/v1'
+import * as R from 'ramda'
+
+import Schema from './Schema'
 
 class Definitions extends React.Component {
   constructor (props) {
@@ -15,29 +18,40 @@ class Definitions extends React.Component {
       return { models: [] }
     }
     return {
-      models: swagger.definitions.keys.map(name => ({
-        uuid: uuidv1(),
-        name,
-        schema: swagger[name]
-      }))
+      models: R.pipe(
+        R.sort,
+        R.map(name => ({
+          uuid: uuidv1(),
+          name,
+          schema: swagger[name]
+        }))
+      )(swagger.definitions.keys)
     }
   }
 
   render () {
     let models = null
     if (this.state.models.length > 0) {
-      models = <Collapse accordion>
+      models = <Collapse accordion activeKey={this.state.activeKey} onChange={targetKey => { this.setState({ activeKey: targetKey }) }}>
         {this.state.models.map(model => (
           <Collapse.Panel header={model.name} key={model.uuid}>
-            {model.name}
+            <Schema schema={model.schema} />
           </Collapse.Panel>))}
       </Collapse>
     }
     return (
       <Card title='Definitions'>
         { models }
-        <Button>Add</Button>
-        <Button>Save</Button>
+        <div style={{ marginTop: '16px' }}>
+          <Button onClick={e => {
+            const uuid = uuidv1()
+            this.setState({
+              activeKey: uuid,
+              models: R.append({ uuid, name: 'ModelName', schema: {} }, this.state.models)
+            })
+          }}><Icon type='plus' />Add</Button>
+          <Button type='primary'><Icon type='save' />Save</Button>
+        </div>
       </Card>
     )
   }
---
+++ src/web/components/Schema.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Schema extends React.Component {
+  render () {
+    return (
+      <div>
+        Schema
+      </div>
+    )
+  }
+}
+
+export default Schema
