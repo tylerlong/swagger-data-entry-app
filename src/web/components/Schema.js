120a5c395531458333ebe2cd4101567ab48ac50a
Tyler Long <tyler4long@gmail.com>
2017-11-23 21:31:24 +0800

    Refactor code

--- src/web/components/Schema.js
+++ src/web/components/Schema.js
@@ -6,18 +6,19 @@ import uuidv1 from 'uuid/v1'
 import { inputLayout } from '../utils'
 import Property from './Property'
 import PropertyModel from '../models/Property'
+import BaseComponent from './BaseComponent'
 
-class Schema extends React.Component {
+class Schema extends BaseComponent {
   constructor (props) {
     super(props)
     this.state = {
-      fields: this.getFields()
+      properties: this.fromStore()
     }
   }
 
-  getFields () {
-    const { schema } = this.props
-    if (schema.properties === undefined) {
+  fromStore () {
+    const { schema: { properties } } = this.props
+    if (properties === undefined) {
       return []
     }
     return R.pipe(
@@ -25,35 +26,34 @@ class Schema extends React.Component {
       R.map(name => ({
         uuid: uuidv1(),
         name,
-        schema: schema.properties.get(name)
+        schema: properties.get(name)
       })),
       R.sortBy(R.prop('name'))
-    )(schema.properties.toJSON())
+    )(properties.toJSON())
   }
 
-  getProperties () {
+  toStore () {
     return R.pipe(
-      R.map(field => ([field.name, field.schema.toJSON()])),
+      R.map(property => ([property.name, property.schema.toJSON()])),
       R.fromPairs
-    )(this.state.fields)
+    )(this.state.properties)
   }
 
   render () {
     const { schema } = this.props
-    let fields = null
-    if (this.state.fields.length > 0) {
-      fields = <Collapse accordion activeKey={this.state.activeKey} onChange={targetKey => { this.setState({ activeKey: targetKey }) }}>
-        {this.state.fields.map(field => (
-          <Collapse.Panel header={field.name} key={field.uuid}>
-            <Popconfirm placement='top' title='Are you sure?' onConfirm={() => { this.setState({ fields: R.reject(f => f.uuid === field.uuid, this.state.fields) }) }} okText='Yes' cancelText='No'>
+    let properties = null
+    if (this.state.properties.length > 0) {
+      properties = <Collapse accordion activeKey={this.state.activeKey} onChange={targetKey => { this.setStateProp('activeKey', targetKey) }}>
+        {this.state.properties.map((property, index) => (
+          <Collapse.Panel header={property.name} key={property.uuid}>
+            <Popconfirm placement='top' title='Are you sure?' okText='Yes' cancelText='No'
+              onConfirm={() => { this.setStateProp('properties', R.remove(index, 1)) }}>
               <Button type='danger'><Icon type='delete' /> Delete</Button>
             </Popconfirm>
             <Form.Item label='Name' {...inputLayout}>
-              <Input defaultValue={field.name} onChange={e => {
-                this.state.fields[R.findIndex(R.propEq('uuid', field.uuid), this.state.fields)].name = e.target.value
-              }} />
+              <Input defaultValue={property.name} onChange={e => { this.state.properties[index].name = e.target.value }} />
             </Form.Item>
-            <Property property={field.schema} />
+            <Property property={property.schema} />
           </Collapse.Panel>
       ))}
       </Collapse>
@@ -72,18 +72,16 @@ class Schema extends React.Component {
             onChange={value => { schema.update('required', value) }} />
         </Form.Item>
         <Card title='Properties'>
-          {fields}
+          {properties}
           <div style={{ marginTop: '16px' }}>
             <Button onClick={e => {
               const uuid = uuidv1()
-              this.setState({
-                activeKey: uuid,
-                fields: R.append({ uuid, name: 'fieldName', schema: PropertyModel.create({}) }, this.state.fields)
-              })
+              this.setStateProp('properties', R.append({ uuid, name: 'propertyName', schema: PropertyModel.create({}) }))
+              this.setStateProp('activeKey', uuid)
             }}><Icon type='plus' />Add</Button>
             <Button type='primary' onClick={e => {
-              schema.update('properties', this.getProperties())
-              this.setState({ fields: this.getFields() })
+              schema.update('properties', this.toStore()) // sync state to store
+              this.setState({ properties: this.fromStore() }) // sync store to state
             }}><Icon type='save' />Save</Button>
           </div>
         </Card>
