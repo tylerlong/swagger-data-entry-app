29525cbcbb09656d667b708e02abdff271641173
Tyler Long <tyler4long@gmail.com>
2017-11-24 17:58:14 +0800

    Sort properties

--- src/web/components/definitions/Properties.js
+++ src/web/components/definitions/Properties.js
@@ -3,6 +3,7 @@ import { observer } from 'mobx-react'
 import { Card, Collapse, Popconfirm, Button, Icon } from 'antd'
 import { getParent } from 'mobx-state-tree'
 import uuidv1 from 'uuid/v1'
+import * as R from 'ramda'
 
 import Property from './Property'
 import BaseComponent from '../common/BaseComponent'
@@ -19,7 +20,7 @@ class Properties extends BaseComponent {
       <Card title='Properties'>
         {properties.size < 1 ? null : (
           <Collapse accordion activeKey={this.state.activeKey} onChange={targetKey => { this.setStateProp('activeKey', targetKey) }}>
-            {properties.entries().map(([name, property]) => (
+            {R.sortBy(R.prop(0), properties.entries()).map(([name, property]) => (
               <Collapse.Panel header={name} key={name}>
                 <Popconfirm placement='top' title='Are you sure?' okText='Yes' cancelText='No'
                   onConfirm={() => { getParent(properties).removeProperty(name) }}>
