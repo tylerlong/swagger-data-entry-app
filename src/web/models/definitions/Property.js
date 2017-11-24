a534999ee8c42a0ba332ffdaa68ff2b9db34cb00
Tyler Long <tyler4long@gmail.com>
2017-11-24 17:30:05 +0800

    Remove schema from Property

--- src/web/models/definitions/Property.js
+++ src/web/models/definitions/Property.js
@@ -8,14 +8,13 @@ const Property = types.model({
   format: types.union(types.string, types.undefined),
   description: types.union(types.string, types.undefined),
   enum: types.union(types.array(types.string), types.undefined),
-  items: types.union(types.late(() => Property), types.undefined),
   default: types.union(types.boolean, types.undefined),
   readOnly: types.union(types.boolean, types.undefined),
   maxLength: types.union(types.number, types.undefined),
   minLength: types.union(types.number, types.undefined),
   pattern: types.union(types.string, types.undefined),
-  additionalProperties: types.union(types.late(() => Property), types.undefined),
-  schema: types.union(types.late(() => Property), types.undefined)
+  items: types.union(types.late(() => Property), types.undefined),
+  additionalProperties: types.union(types.late(() => Property), types.undefined)
 }).actions(self => ({
   replace: replace(self),
   update: update(self)
