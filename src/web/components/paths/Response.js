2c28716de71efb5076b4f264cd702245969cc846
Tyler Long <tyler4long@gmail.com>
2017-11-26 20:02:01 +0800

    Make Response work

--- src/web/components/paths/Response.js
+++ src/web/components/paths/Response.js
@@ -1,9 +1,58 @@
 import React from 'react'
+import { Input, Icon, Button, Card } from 'antd'
+import { getParent } from 'mobx-state-tree'
+import { observer } from 'mobx-react'
+
+import RequiredFields from '../common/RequiredFields'
+import OptionalFields from '../common/OptionalFields'
+import Property from '../definitions/Property'
 
 class Response extends React.Component {
+  constructor (props) {
+    super(props)
+    this.form = {}
+    const { name, response } = props
+    this.requiredFields = {
+      name: <Input defaultValue={name} onChange={e => { this.form.name = e.target.value }} />,
+      description: <Input defaultValue={response.description} onChange={e => { this.form.description = e.target.value }} />
+    }
+    if (!name) {
+      delete this.requiredFields.name
+    }
+    this.optionalFields = {
+      schema: () => <Card><Property property={response.schema} /></Card>,
+      examples: () => 'Examples'
+    }
+    this.defaultValues = {
+      schema: {},
+      examples: {}
+    }
+    this.tooltips = {
+      name: 'Such as "default", "200", "404"...'
+    }
+  }
+
   render () {
-    return 'Response'
+    const { name, response } = this.props
+    return (
+      <div>
+        <Button onClick={() => {
+          response.replace(this.form)
+          if (name && this.form.name) {
+            getParent(getParent(response)).renameResponse(name, this.form.name)
+          }
+        }}><Icon type='save' /> Save</Button>
+        <RequiredFields
+          requiredFields={this.requiredFields}
+          tooltips={this.tooltips} />
+        <OptionalFields
+          optionalFields={this.optionalFields}
+          defaultValues={this.defaultValues}
+          model={response}
+          form={this.form} />
+      </div>
+    )
   }
 }
 
-export default Response
+export default observer(Response)
--- src/web/models/paths/Response.js
+++ src/web/models/paths/Response.js
@@ -1,11 +1,15 @@
 import { types } from 'mobx-state-tree'
 
 import Property from '../definitions/Property'
+import { update, replace } from '../../utils'
 
 const Response = types.model({
   description: types.string,
   schema: types.union(Property, types.undefined),
   examples: types.union(types.map(types.string), types.undefined)
-})
+}).actions(self => ({
+  update: update(self),
+  replace: replace(self)
+}))
 
 export default Response
