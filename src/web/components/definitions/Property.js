c602dd01832b8d1fe5f5d6bb3e812381bfdf8061
Tyler Long <tyler4long@gmail.com>
2017-11-26 14:32:23 +0800

    Rewrite Property component

--- src/web/components/definitions/Property.js
+++ src/web/components/definitions/Property.js
@@ -1,91 +1,81 @@
 import React from 'react'
-import { Form, Input, Icon, Button, Select, Card, Checkbox, InputNumber } from 'antd'
+import { Form, Input, Icon, Button, Select, Checkbox, InputNumber, Card } from 'antd'
 import { getParent } from 'mobx-state-tree'
 import { observer } from 'mobx-react'
 
 import { inputLayout, buttonLayout } from '../../utils'
+import OptionalFields from '../common/OptionalFields'
 
 class Property extends React.Component {
   constructor (props) {
     super(props)
     this.form = {}
+    const { property } = props
+    this.optionalFields = {
+      $ref: () => <Input defaultValue={property.$ref} onChange={e => { this.form.$ref = e.target.value }} />,
+      type: () => <Input defaultValue={property.type} onChange={e => { this.form.type = e.target.value }} />,
+      format: () => <Input defaultValue={property.format} onChange={e => { this.form.format = e.target.value }} />,
+      description: () => <Input defaultValue={property.description} onChange={e => { this.form.description = e.target.value }} />,
+      enum: () => <Select placeholder='Input some text then press enter' mode='tags' style={{ width: '100%' }}
+        defaultValue={property.enum.toJSON()} onChange={value => { this.form.enum = value }} />,
+      default: () => <Input defaultValue={property.default} onChange={e => {
+        let value = e.target.value
+        if (value === 'true' || value === 'yes') {
+          value = true
+        } else if (value === 'false' || value === 'no') {
+          value = false
+        }
+        this.form.default = value
+      }} />,
+      readOnly: () => <Checkbox onChange={e => { this.form.readOnly = e.target.checked }}>Checkbox</Checkbox>,
+      maxLength: () => <InputNumber min={0} defaultValue={1000} onChange={value => { this.form.maxLength = value }} />,
+      minLength: () => <InputNumber min={0} defaultValue={0} onChange={value => { this.form.minLength = value }} />,
+      pattern: () => <Input defaultValue={property.pattern} onChange={e => { this.form.pattern = e.target.value }} />,
+      items: () => <Card><Property property={property.items} /></Card>,
+      additionalProperties: () => <Card><Property property={property.additionalProperties} /></Card>
+    }
+    this.defaultValues = {
+      $ref: '',
+      type: 'string',
+      format: '',
+      description: '',
+      enum: [],
+      default: '',
+      readOnly: false,
+      maxLength: 1000,
+      minLength: 0,
+      pattern: '',
+      items: {},
+      additionalProperties: {}
+    }
+    this.tooltips = {
+      default: '"true", "yes", "false" and "no" will be converted to boolean'
+    }
   }
 
   render () {
-    const { name, property, fixedName } = this.props
-    let items = null
-    if (property.items) {
-      items = <Property name='items' property={property.items} fixedName />
-    } else {
-      items = <Button onClick={() => property.newProperty('items')}>Add</Button>
-    }
-    let additionalProperties = null
-    if (property.additionalProperties) {
-      additionalProperties = <Property name='additionalProperties' property={property.additionalProperties} fixedName />
-    } else {
-      additionalProperties = <Button onClick={() => property.newProperty('additionalProperties')}>Add</Button>
-    }
+    const { name, property } = this.props
     return (
       <div>
-        {fixedName ? null : (
+        {name === undefined ? null : (
           <Form.Item label='Name' {...inputLayout}>
             <Input defaultValue={name} onChange={e => { this.form.name = e.target.value }} />
           </Form.Item>
         )}
-        <Form.Item label='$ref' {...inputLayout}>
-          <Input defaultValue={property.$ref} onChange={e => { this.form.$ref = e.target.value }} />
-        </Form.Item>
-        <Form.Item label='Type' {...inputLayout}>
-          <Input defaultValue={property.type} onChange={e => { this.form.type = e.target.value }} />
-        </Form.Item>
-        <Form.Item label='Format' {...inputLayout}>
-          <Input defaultValue={property.format} onChange={e => { this.form.format = e.target.value }} />
-        </Form.Item>
-        <Form.Item label='Description' {...inputLayout}>
-          <Input defaultValue={property.description} onChange={e => { this.form.description = e.target.value }} />
-        </Form.Item>
-        <Form.Item label='Enum' {...inputLayout}>
-          <Select placeholder='Input some text then press enter' mode='tags' style={{ width: '100%' }}
-            defaultValue={property.enum ? property.enum.toJSON() : []}
-            onChange={value => { this.form.enum = value }} />
-        </Form.Item>
-        <Form.Item label='Default' {...inputLayout}>
-          <Input defaultValue={property.default} onChange={e => {
-            let value = e.target.value
-            if (value === 'true') {
-              value = true
-            } else if (value === 'false') {
-              value = false
-            }
-            this.form.default = value
-          }} />
-        </Form.Item>
-        <Form.Item label='ReadOnly' {...inputLayout}>
-          <Checkbox onChange={e => { this.form.readOnly = e.target.checked ? true : '' }}>Checkbox</Checkbox>
-        </Form.Item>
-        <Form.Item label='MaxLength' {...inputLayout}>
-          <InputNumber min={0} defaultValue={1000} onChange={value => { this.form.maxLength = value }} />
-        </Form.Item>
-        <Form.Item label='MinLength' {...inputLayout}>
-          <InputNumber min={0} defaultValue={0} onChange={value => { this.form.minLength = value }} />
-        </Form.Item>
-        <Form.Item label='Pattern' {...inputLayout}>
-          <Input defaultValue={property.pattern} onChange={e => { this.form.pattern = e.target.value }} />
-        </Form.Item>
+        <OptionalFields
+          optionalFields={this.optionalFields}
+          defaultValues={this.defaultValues}
+          model={property}
+          form={this.form}
+          tooltips={this.tooltips} />
         <Form.Item {...buttonLayout}>
           <Button onClick={() => {
             property.replace(this.form)
-            if (!fixedName && this.form.name) {
+            if (name && this.form.name) {
               getParent(getParent(property)).renameProperty(name, this.form.name)
             }
           }}><Icon type='save' /> Save</Button>
         </Form.Item>
-        <Card title='Items'>
-          {items}
-        </Card>
-        <Card title='AdditionalProperties'>
-          {additionalProperties}
-        </Card>
       </div>
     )
   }
