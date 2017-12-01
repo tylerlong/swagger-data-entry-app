34f804eea9138cecb7941532ea18bf13e552ce50
Tyler Long <tyler4long@gmail.com>
2017-12-01 17:17:44 +0800

    Fix default type

--- src/web/models/paths/Parameter.js
+++ src/web/models/paths/Parameter.js
@@ -12,7 +12,7 @@ const Parameter = types.model({
   required: types.union(types.boolean, types.undefined),
   type: types.union(types.enumeration(primitiveTypes), types.undefined),
   enum: types.union(types.array(types.string), types.undefined),
-  default: types.union(types.union(types.string, types.boolean, types.number), types.undefined),
+  default: types.union(types.string, types.boolean, types.number, types.undefined),
   collectionFormat: types.union(types.enumeration(collectionFormats), types.undefined),
   maximum: types.union(types.number, types.undefined),
   minimum: types.union(types.number, types.undefined),
