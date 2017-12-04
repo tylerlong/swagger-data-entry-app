5176551e9acd4868cc1a42c7c46ad44647f0b1db
Tyler Long <tyler4long@gmail.com>
2017-12-04 10:01:46 +0800

    Refactor code

--- src/web/models/paths/Operation.js
+++ src/web/models/paths/Operation.js
@@ -3,9 +3,10 @@ import * as R from 'ramda'
 
 import Parameter from './Parameter'
 import Response from './Response'
-import { update, replace, extensionFieldActions } from '../../utils'
+import { update, replace } from '../../utils'
+import Extensions from './Extensions'
 
-const Operation = types.model({
+let Operation = types.model({
   tags: types.union(types.array(types.string), types.undefined),
   summary: types.union(types.string, types.undefined),
   description: types.union(types.string, types.undefined),
@@ -14,19 +15,10 @@ const Operation = types.model({
   consumes: types.union(types.array(types.string), types.undefined),
   produces: types.union(types.array(types.string), types.undefined),
   parameters: types.union(types.array(Parameter), types.undefined),
-  responses: types.map(Response),
-  'x-extension-fields': types.union(types.map(types.union(types.string, types.boolean, types.number)), types.undefined)
-}).views(self => ({
-  extensionField (name) {
-    if (self['x-extension-fields'] === undefined) {
-      return undefined
-    }
-    return self['x-extension-fields'].get(name)
-  }
-})).actions(self => ({
+  responses: types.map(Response)
+}).actions(self => ({
   update: update(self),
   replace: replace(self),
-  ...extensionFieldActions(self),
   newParameter (uuid) {
     self.parameters.push({ name: uuid, in: 'path' })
   },
@@ -50,5 +42,7 @@ const Operation = types.model({
     self.responses.delete(name)
   }
 }))
+
+Operation = types.compose(Operation, Extensions)
 
 export default Operation
--- src/web/models/paths/PathItem.js
+++ src/web/models/paths/PathItem.js
@@ -1,26 +1,20 @@
 import { types } from 'mobx-state-tree'
 
 import Operation from './Operation'
-import { update, replace, extensionFieldActions } from '../../utils'
+import { update, replace } from '../../utils'
+import Extensions from './Extensions'
 
-const PathItem = types.model({
+let PathItem = types.model({
   get: types.union(Operation, types.undefined),
   post: types.union(Operation, types.undefined),
   put: types.union(Operation, types.undefined),
   delete: types.union(Operation, types.undefined),
-  head: types.union(Operation, types.undefined),
-  'x-extension-fields': types.union(types.map(types.union(types.string, types.boolean, types.number)), types.undefined)
-}).views(self => ({
-  extensionField (name) {
-    if (self['x-extension-fields'] === undefined) {
-      return undefined
-    }
-    return self['x-extension-fields'].get(name)
-  }
-})).actions(self => ({
+  head: types.union(Operation, types.undefined)
+}).actions(self => ({
   update: update(self),
-  replace: replace(self),
-  ...extensionFieldActions(self)
+  replace: replace(self)
 }))
+
+PathItem = types.compose(PathItem, Extensions)
 
 export default PathItem
