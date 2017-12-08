62d894b50eda6eaa866a13d5ec959d2fc0116180
Tyler Long <tyler4long@gmail.com>
2017-12-08 13:24:59 +0800

    Replace arguments with ...args

--- src/web/components/common/BaseComponent.js
+++ src/web/components/common/BaseComponent.js
@@ -2,22 +2,22 @@ import React from 'react'
 import * as R from 'ramda'
 
 class BaseComponent extends React.Component {
-  setStateProp () {
-    if (this.state === undefined || arguments.length < 2) {
+  setStateProp (...args) {
+    if (this.state === undefined || args.length < 2) {
       return
     }
-    let value = R.last(arguments)
+    let value = R.last(args)
     if (typeof value === 'function') {
-      value = value(R.view(R.lensPath(R.init(arguments)), this.state))
+      value = value(R.view(R.lensPath(R.init(args)), this.state))
     }
-    if (R.any(item => typeof item === 'number', R.init(arguments))) { // array structure involved
-      const newState = R.set(R.lensPath(R.init(arguments)), value, this.state)
-      const shortIndex = R.findIndex(item => typeof item === 'number', arguments)
-      const shortPath = R.lensPath(R.slice(0, shortIndex, arguments))
+    if (R.any(item => typeof item === 'number', R.init(args))) { // array structure involved
+      const newState = R.set(R.lensPath(R.init(args)), value, this.state)
+      const shortIndex = R.findIndex(item => typeof item === 'number', args)
+      const shortPath = R.lensPath(R.slice(0, shortIndex, args))
       const delta = R.set(shortPath, R.view(shortPath, newState), {})
       this.setState(delta)
     } else {
-      this.setState(R.set(R.lensPath(R.init(arguments)), value, {}))
+      this.setState(R.set(R.lensPath(R.init(args)), value, {}))
     }
   }
 }
